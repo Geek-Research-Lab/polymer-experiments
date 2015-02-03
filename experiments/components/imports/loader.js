6a70979eeaf7d70435982b0018792626859c37e7
Aswini S <ashumeow@live.com>
2015-02-03 18:14:33 +0530

    add imports

---
+++ experiments/components/imports/loader.js
@@ -0,0 +1,15 @@
+function loaderMix(gems) {
+	'use strict';
+
+	// importing
+	var xhr = ['xhr.js'];
+	xhr = xhr.gems;
+	var flags = gems.flags;
+
+	// main
+	var loader = function(onLoad, onComplete) {
+		//
+		// Still more to code!
+		//
+	};
+}
