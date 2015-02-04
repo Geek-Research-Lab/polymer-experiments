b090cddc4adb1dda48bc57e3901fec613f0a3bc9
Aswini S <ashumeow@live.com>
2015-02-04 22:35:02 +0530

    imports

---
+++ experiments/tests/imports/imports.js
@@ -0,0 +1,19 @@
+function imports() {
+
+	// loading file
+	var file = 'imports.js';
+
+	// loading JS files/modules
+	var modules = [
+	'preload.js',
+	'xhr.js',
+	'loader.js'
+	];
+
+	// source
+	var src = document.querySelector('script[src*="' + file + '"]').getAttribute('src');
+
+	//
+	// Still more to code!
+	//
+}
