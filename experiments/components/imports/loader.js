00c1b4c0ab6bd3d8079371e6a75b7d78df1c43f3
Aswini S <ashumeow@live.com>
2015-02-04 06:32:13 +0530

    Update loader.js

--- experiments/components/imports/loader.js
+++ experiments/components/imports/loader.js
@@ -5,26 +5,26 @@ function loaderMix(gems) {
 	xhr = xhr.gems;
 	var flags = gems.flags;
 
-	var ig = this;
-
 	// main
 	var loader = function(e) {
 		'use strict';
 		// u => url
 		// e => element
 		// nu => node url
-		// dd => deduplication
+		// dd => de-duplication
 		// f => fetch
+		var ig = this;
     	var u = e.src || e.href;
     	e.nu = u;
     	if (!ig.dd(u, e)) {
       		// fetching the resource
       		ig.f(u, e);
     	}
-  	};
-  	//
+
+    //
   	// Still more to code!
   	//
+  	};
 
 	// exports
 	gems.loader = loader;
