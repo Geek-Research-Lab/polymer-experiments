18aab2d408c31a622b0c8884cd7324b5ce315a07
Aswini S <ashumeow@live.com>
2015-02-04 11:23:21 +0530

    Update loader.js

--- experiments/components/imports/loader.js
+++ experiments/components/imports/loader.js
@@ -20,7 +20,43 @@ function loaderMix(gems) {
       	// fetching the resource
       	ig.f(u, e);
     	}
-    	
+
+    	function f(u, e) {
+    		// ff => flags
+    		ff.load();
+    		console.log('fetch', u, e);
+    		if(!u) {
+    			setTimeout(function() {
+    			// rc => receive
+    			ig.rc(u, e, {error: 'href must be specified'}, null);
+    		}.bind(ig), 0);
+    		} else if(u.match(/^data:/)) {
+    		// handling data URI scheme
+    		// p => pieces
+    		// h => header
+    		// b => body
+    		var p = u.split(',');
+    		var h = p[0];
+    		var b = p[1];
+    		if(h.indexOf(';base64') > -1) {
+    			b = atob(b);
+    		} else {
+    			b = decodeURIComponent(b);
+    		}
+    		setTimeout(function() {
+    			ig.rc(u, e, null, b);
+    		}.bind(ig), 0);
+    		} else {
+    		// rx => Receiving XHR
+    		// r => resource
+    		// ru => redirected url
+    		var rx = function(err, r, ru) {
+    			ig.rc(u, e, err, r, ru);
+    		}.bind(ig);
+    		xhr.load(u, rx);
+    		}
+    	}
+
     	//
   	// Still more to code!
   	//
