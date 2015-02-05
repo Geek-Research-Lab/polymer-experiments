8c5f5dcd379223e150589e7246cd7cf6e66ff7aa
Aswini S <ashumeow@live.com>
2015-02-05 19:30:13 +0530

    Update WeakMap.js

--- experiments/components/WeakMap.js
+++ experiments/components/WeakMap.js
@@ -3,6 +3,24 @@
 
 var WeakMap = function() {
 	'use strict';
+	/* MODULES
+	-- set
+	-- get
+	-- delete
+	-- has
+	////////
+	1. WeakMap.prototype.constructor
+	2. WeakMap.prototype.delete(key)
+	3. WeakMap.prototype.get(key)
+	4. WeakMap.prototype.has(key)
+	5. WeakMap.prototype.set(key, value)
+	6. WeakMap.prototype[@@toStringTag]
+	////////
+	// reference:-
+	WeakMap.prototype
+	http://people.mozilla.org/~jorendorff/es6-draft.html#sec-weakmap.prototype
+	//////////////
+	*/
 	//
 	// Still more to code!
 	//
