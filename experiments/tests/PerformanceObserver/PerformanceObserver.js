d3b625ecd6d9b1e0e8cd2f69704e2b92ea569494
Aswini S <ashumeow@live.com>
2015-02-06 05:15:06 +0530

    added

---
+++ experiments/tests/PerformanceObserver/PerformanceObserver.js
@@ -0,0 +1,58 @@
+var PerformanceObserver = function() {
+	'use strict';
+	/* MODULES
+	-- observe
+	-- disconnect
+	*/
+	var perf = this;
+	PerformanceObserver.prototype = {
+	// observe
+	observe: function(target, opts) {
+		var events = table.get(target);
+		if(!events) {
+			table.set(target, events = []);
+		}
+		var Event;
+		for(var m = 0; m < events.length; m++) {
+			if(events[m].observer === perf) {
+				Event = events[m];
+				Event.removeListeners();
+				Event.options = opts;
+				break;
+			}
+		}
+		if(!Event) {
+			Event = new Event(perf, target, opts);
+			events.push(Event);
+			perf.push(target);
+		}
+		Event.addListeners();
+	},
+	// disconnect
+	disconnect: function() {
+		perf.forEach(function(key) {
+			var events = table.get(key);
+			for(var m = 0; m < events.length; m++) {
+				var Event = events[m];
+				if(Event.observer === perf) {
+					Event.removeListeners();
+					events.splice(m, 1);
+					break;
+				}
+			}
+		}, perf);
+	} };
+};
+
+/* References:-
+[1] http://w3c.github.io/performance-timeline/#sec-performance-timeline
+[2] http://addyosmani.com/blog/the-future-of-data-binding-is-object-observe/
+*/
+/* Other References:-
+[1] https://dom.spec.whatwg.org/#mutation-observers
+[2] https://code.google.com/p/mutation-summary/
+[3] http://updates.html5rocks.com/2012/02/Detect-DOM-changes-with-Mutation-Observers
+[4] http://hacks.mozilla.org/2012/05/dom-mutationobserver-reacting-to-dom-changes-without-killing-browser-performance/
+[5] https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver?redirectlocale=en-US&redirectslug=DOM%2FMutationObserver
+[6] https://github.com/webcomponents/webcomponentsjs/blob/master/src/MutationObserver/MutationObserver.js
+*/
