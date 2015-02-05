0fa69c86348b9a9f3fe3f853097382f4829cbdf5
Aswini S <ashumeow@live.com>
2015-02-05 22:10:51 +0530

    Update WeakMap.js

--- experiments/components/WeakMap.js
+++ experiments/components/WeakMap.js
@@ -21,6 +21,7 @@ var WeakMap = function() {
 	*/
 	var ig = this;
 	WeakMap.prototype = {
+
 		// set
 		// WeakMap.prototype.set(k, v)
 		set: function(k, v) {
@@ -35,12 +36,25 @@ var WeakMap = function() {
 			}
 			return ig;
 		},
+
 		// get
 		// WeakMap.prototype.get(k)
 		get: function(k) {
 			var ent;
 			return (ent = k[ig.name]) && ent[0] === k ? ent[1] : undefined;
 		},
+
+		// delete
+		// WeakMap.prototype.delete(k)
+		delete: function(k) {
+			var ent = k[ig.name];
+			if(!ent || ent[0] !== k) {
+				return false;
+			}
+			ent[0] = ent[1] = undefined;
+			return true;
+		}
+
 	//
 	// Still more to code!
 	//
@@ -51,4 +65,17 @@ var WeakMap = function() {
 Credits
 [1] http://people.mozilla.org/~jorendorff/es6-draft.html#sec-weakmap-constructor
 [2] https://github.com/webcomponents/webcomponentsjs/blob/master/src/WeakMap/WeakMap.js
+*/
+
+/*
+// TODO
+PDFs to read:-
+[1] http://www.jucs.org/jucs_14_21/eliminating_cycles_in_weak/jucs_14_21_3481_3497_barros.pdf
+
+Other Links to explore:- // TODO
+[1] http://docs.racket-lang.org/reference/ephemerons.html
+[2] https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/MemoryMgmt/Articles/mmPractical.html
+[3] http://weblog.ikvm.net/PermaLink.aspx?guid=7f47ad08-cdef-4dc2-b2fd-5dfdc1baf11d
+[4] https://msdn.microsoft.com/en-us/library/dd287757(v=vs.100).aspx
+[5] http://www.gnu.org/software/mit-scheme/documentation/mit-scheme-ref/Ephemerons.html
 */
