798c6093c8230d549acfe6bf5fd525ef5637bd5f
Aswini S <ashumeow@live.com>
2015-02-05 19:51:58 +0530

    add component

---
+++ experiments/components/MutationObserver.js
@@ -0,0 +1,16 @@
+var MutationObserver = function() {
+	'use strict';
+
+	//
+	// Still more to code
+	//
+};
+
+/* References:-
+[1] https://dom.spec.whatwg.org/#mutation-observers
+[2] https://code.google.com/p/mutation-summary/
+[3] http://updates.html5rocks.com/2012/02/Detect-DOM-changes-with-Mutation-Observers
+[4] http://hacks.mozilla.org/2012/05/dom-mutationobserver-reacting-to-dom-changes-without-killing-browser-performance/
+[5] https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver?redirectlocale=en-US&redirectslug=DOM%2FMutationObserver
+[6] https://github.com/webcomponents/webcomponentsjs/blob/master/src/MutationObserver/MutationObserver.js
+*/
