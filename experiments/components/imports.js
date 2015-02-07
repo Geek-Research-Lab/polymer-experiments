840dd8a7e5f5cffcc8b1e02a15441b004c6cae87
Aswini S <ashumeow@live.com>
2015-02-07 22:35:01 +0530

    Update imports.js

--- experiments/components/imports.js
+++ experiments/components/imports.js
@@ -15,7 +15,9 @@ function imports() {
 	it is a lot similar to booting a system..
 	I always had an intention to turn OS into Web
 	And Kernel is the core of OS
-	Always dreamed of building a web kernel ever for a long time
+	Always dreamed of building a web kernel for a long time
+	We can build anything with a kernel =P
+	Wish i had a genius brain! =D =D
 	Every web browser has an interface with OS
 	In between the interface is the loophole =D
 	*/
