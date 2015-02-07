808fd9e4fe0895f8fbca1159d1b65bb961963f5c
Aswini S <ashumeow@live.com>
2015-02-07 21:43:40 +0530

    Update path.js

--- experiments/components/imports/path.js
+++ experiments/components/imports/path.js
@@ -9,7 +9,41 @@ module.addModule = function(gems) {
 
 var path = function() {
 	'use strict';
-	//
-	// Still more to code
-	//
-}; };
+	var ig = this;
+	// Resolving URLs in Style
+	// rs => Resolving URLs in Style
+	// st => style
+	// re => resolver
+	// rct => Resolving URLs in CSS Text
+	rs: function(st) {
+		var doc = st.ownerDocument;
+		var re = doc.createElement('a');
+		st.textContent = ig.rct(st.textContent, re);
+		return st;
+	},
+	// Resolving URLs in CSS Text
+	rct: function(ct, uo) {
+		// ct => CSS Text
+		// uo => URL Object
+		//rurl => Replace URLs
+		// txt => Text
+		// rgx => RegExp
+		// u => url
+		// up => url path
+		var re = ig.rurl(ct, uo, css_url_regex);
+		re = ig.rurl(re, uo, css_import_regex);
+		return re;
+	},
+	// Replacing URLs
+	rurl: function(txt, uo, rgx) {
+		return txt.replace(rgx, function(mm, pre, u, post) {
+			var up = u.replace(/["']/g, '');
+			uo.href = up;
+			up = uo.href;
+			return pre + '\'' + up + '\'' + post;
+		});
+	}
+}; 
+// exporting
+gems.path = path;
+};
