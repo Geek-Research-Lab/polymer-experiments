0faecd7034f42d3e3c7b16ee592faa9463fd144e
Aswini S <ashumeow@live.com>
2015-02-07 22:45:56 +0530

    Update imports.js

--- experiments/tests/imports/imports.js
+++ experiments/tests/imports/imports.js
@@ -7,7 +7,20 @@ function imports() {
 	var modules = [
 	'preload.js',
 	'xhr.js',
-	'loader.js'
+	'loader.js',
+	'path.js',
+	// 'module.js',
+	/*
+	In module.js, it has addModule and InitModule...
+	it is a lot similar to booting a system..
+	I always had an intention to turn OS into Web
+	And Kernel is the core of OS
+	Always dreamed of building a web kernel for a long time
+	We can build anything with a kernel =P
+	Wish i had a genius brain! =D =D
+	Every web browser has an interface with OS
+	In between the interface is the loophole =D
+	*/
 	];
 
 	// source
