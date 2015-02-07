83750978c531aabf919c7b4b59d3085ff80c10e6
Aswini S <ashumeow@live.com>
2015-02-07 21:24:50 +0530

    Update path.js

--- experiments/components/imports/path.js
+++ experiments/components/imports/path.js
@@ -1,6 +1,15 @@
+// Path
+
+// loading module
+var module = ['module.js'];
+
+module.addModule = function(gems) {
+	var css_url_regex = /(url\()([^)]*)(\))/g;
+	var css_import_regex = /(@import[\s]+(?!url\())([^;]*)(;)/g;
+
 var path = function() {
 	'use strict';
 	//
 	// Still more to code
 	//
-};
+}; };
